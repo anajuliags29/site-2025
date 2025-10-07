6078c70c2a93cfa21a4a4e022ce4218f71fc8179
anajuliags29 <anajuliaguerra010@gmail.com>
2025-10-07 08:32:11 -0300

    correcao do codigo

---
+++ portal/edt_plano.php
@@ -0,0 +1,89 @@
+<?php
+include('verifica_login.php');
+include('conexao.php');
+
+$niveldapagina = array($mestre, $colaborador);
+ $id_plano      = filter_input(INPUT_GET, 'id_plano', FILTER_SANITIZE_NUMBER_INT);
+
+if (!in_array($nivel, $niveldapagina)) {
+   
+    echo "
+        <script>
+            alert('você nao tem permissao para acessar essa area!');
+            history.go(-1);
+        </script>
+    ";
+
+} else if (!empty($id_plano)) {
+        $consulta = "SELECT * FROM planos
+                    WHERE id_plano = '$id_plano'";
+
+        $con     = $conexao->query($consulta) or die ($conexao->error);
+        while($dado = $con->fetch_array()){
+                $nome_plano  = $dado['nome_plano'];   
+                $desc_plano = $dado['desc_plano'];   
+                $vlr_plano = $dado['vlr_plano'];        
+
+} 
+} else {
+
+
+ }
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head> 
+        <title> INÍCIO | Meu Portal </title>
+        <!------------------------------------ HEAD ------------------------------------>
+        <?php include 'include-portal/head.php'; ?>
+        <!------------------------------------ HEAD ------------------------------------>
+    </head>
+    <body>
+        <header>
+            <!--------------------------------- HEADER --------------------------------->
+            <?php include 'include-portal/header.php'; ?>
+            <!--------------------------------- HEADER --------------------------------->
+        </header>
+        
+        <div class="container">
+            <br>
+
+            <center><h5> PREENCHA OS CAMPOS ABAIXO PARA ADICIONAR UM NOVO PLANO </h5></center>
+
+            <hr>
+
+            <?php
+               if (isset ($_SESSION['msgFormPlano'])) {
+                echo $_SESSION['msgFormPlano'];
+                unset($_SESSION['msgFormPlano']);
+
+                
+
+               }
+            ?>
+
+            <form action= "save_plano.php" method="POST">
+                <div class="form-row">
+                    <div class="col">
+                        <label for="nomePlano">* Nome</label>
+                        <input type="text" class="form-control" id= "nome_plano" name="nome_plano" value="<?php echo $nome_plano; ?>">
+                    </div>
+                    <div class="col">
+                        <label for="descricaoPlano">* Descrição</label>
+                        <input type="text" class="form-control" id="desc_plano"  name="desc_plano" value="<?php echo $desc_plano; ?>">
+                    </div>
+                    <div class="col">
+                        <label for="valorPlano">* Valor</label>
+                        <input type="text" class="form-control" id="vlr_plano" name="vlr_plano" value="<?php echo $vlr_plano; ?>">
+                        <button type="reset" class="btn btn-danger">ENVIAR</button> 
+                    </div>.
+                        
+                </div>
+            </form>
+            <br>
+            
+        </div>
+        <footer>
---
+++ portal/save_plano.php
@@ -0,0 +1,43 @@
+<?php
+include('verifica_login.php');
+include('conexao.php');
+
+$niveldapagina = array($mestre, $colaborador);
+
+if (!in_array($nivel, $niveldapagina)) {
+    echo "
+        <script>
+            alert('você nao tem permissao para acessar essa area!');
+            history.go(-1);
+        </script>
+    ";
+} else if (!empty($_POST['nome_plano']) || !empty($_POST['desc_plano']) || !empty($_POST['vlr_plano'])) {
+
+
+    $nome_plano = $_POST['nome_plano'];
+    $desc_plano = $_POST['desc_plano'];
+    $vlr_plano = $_POST['vlr_plano'];
+    
+     $consultaPlano = "SELECT * FROM planos
+                     WHERE id_plano = '$id_plano'";
+
+    $sql_code = "UPDATE  planos SET (nome_plano ='$nome_plano', desc_plano ='$desc_plano', vlr_plano='$vlr_plano' WHERE id_plano = ''";
+
+    if ($conexao->query($sql_code) or die($conexao->error)) {
+       $_SESSION['msgListaPlano'] = "<center><div class='alert alert-success' role='alert'>ok, plano inserido com sucesso!</center>";
+       header("Location: lista_plano.php");
+    } else {
+        $_SESSION['msgListaPlano'] = "<center><div class='alert alert-danger' role='alert'>ERRO, plano nao foi inserido!</center>";
+         header("Location: lista_plano.php");
+    }
+
+} else {
+    echo " 
+        <script>
+            alert('VIOLAÇÃO: Tentativa ilegal de operação');
+            history.go(-1);
+        </script>
+    ";
+}
+
+?>
